7c7c3648d153c1ef0be63a5dffce6943f85c4b4b
Christopher Vollick <psycotica0@gmail.com>
2012-10-28 13:16:02 -0400

    Removed all Qualified Imports
    I've changed my opinion.
    I used to qualify it, but now I've decided being explicit about what I
    import is better, so I've switched to that.
    
    Now I should still have no 'accidents', but I don't have to put letters
    in front of everything.
    
    I like that.

--- intersection-test.hs
+++ intersection-test.hs
@@ -1,8 +1,8 @@
-import qualified Data.Packed.Matrix as M
-import qualified Data.Packed.Vector as V
-import qualified Numeric.LinearAlgebra as A
-import qualified Numeric.LinearAlgebra.Util as U
-import qualified Data.List as L
+import Data.Packed.Matrix (fromColumns, asColumn, (@@>))
+import Data.Packed.Vector (Vector, buildVector, foldVector, (|>), fromList)
+import Numeric.LinearAlgebra (linearSolve, det, pinv, (<>))
+import Numeric.LinearAlgebra.Util (cross)
+import Data.List (intercalate)
 import Data.Ix (range)
 import Data.Tuple (swap)
 import Data.Function (on)
@@ -12,44 +12,44 @@ valueTrace a = traceShow a a
 
 approxZero i = i < 1e-6 && i > -1e-6
 
-scalar a = V.buildVector 3 $ const a
+scalar a = buildVector 3 $ const a
 (|*) a b = (scalar a) * b
 
 -- This represents a plane of the form p = ax + by + c
-data Plane a = Plane (V.Vector a) (V.Vector a) (V.Vector a) deriving (Show)
+data Plane a = Plane (Vector a) (Vector a) (Vector a) deriving (Show)
 
 calc_plane (Plane a b c) n1 n2 = (n1 |* a) + (n2 |* b) + c
 
 -- This represents a ray of the form r = ax + b
-data Ray a = Ray (V.Vector a) (V.Vector a) deriving (Show)
+data Ray a = Ray (Vector a) (Vector a) deriving (Show)
 
 calc_ray (Ray a b) n1 = (n1 |* a) + b
 
 -- Each of these Vectors represent a point in 3-space
-data Triangle a = Triangle (V.Vector a) (V.Vector a) (V.Vector a) deriving (Show)
+data Triangle a = Triangle (Vector a) (Vector a) (Vector a) deriving (Show)
 
-build_triangle (a1, b1, c1) (a2, b2, c2) (a3, b3, c3) = Triangle (3 V.|> [a1, b1, c1]) (3 V.|> [a2,b2,c2]) (3 V.|> [a3,b3,c3])
+build_triangle (a1, b1, c1) (a2, b2, c2) (a3, b3, c3) = Triangle (3 |> [a1, b1, c1]) (3 |> [a2,b2,c2]) (3 |> [a3,b3,c3])
 
 triangle_to_plane (Triangle p1 p2 p3) = Plane (p2 - p1) (p3 - p1) p1
 
 ray_plane_intersection ray@(Ray r1 r2) plane@(Plane p1 p2 p3) = result undefined
 	where
-	mat = M.fromColumns [p1, p2, -1 |* r1]
-	solns = A.linearSolve mat (M.asColumn (r2 - p3))
-	n = solns M.@@> (2,0)
+	mat = fromColumns [p1, p2, -1 |* r1]
+	solns = linearSolve mat (asColumn (r2 - p3))
+	n = solns @@> (2,0)
 	-- The parameter here is a hack to get around the fact that it doesn't pattern match if there are no parameters
-	result fake | approxZero $ A.det mat = Nothing
+	result fake | approxZero $ det mat = Nothing
 	result fake = Just $ calc_ray ray n
 
 ray_triangle_intersect ray triangle@(Triangle p1 p2 p3) = result undefined
 	where
 	intersect = ray_plane_intersection ray plane
 	plane@(Plane v1 v2 v3) = triangle_to_plane triangle
-	invmat = A.pinv $ M.fromColumns [v1, v2]
-	offset = invmat A.<> p1
-	transform vec = (invmat A.<> vec) - offset
+	invmat = pinv $ fromColumns [v1, v2]
+	offset = invmat <> p1
+	transform vec = (invmat <> vec) - offset
 	result fake | intersect == Nothing = False
-	result fake = (V.foldVector (\x acc -> acc && (x > 0)) True transformedPosition) && (1 > (V.foldVector (+) 0 transformedPosition))
+	result fake = (foldVector (\x acc -> acc && (x > 0)) True transformedPosition) && (1 > (foldVector (+) 0 transformedPosition))
 		where
 		Just point = intersect
 		transformedPosition = transform point
@@ -69,26 +69,26 @@ cube v1 v2 v3 p1 = concatMap (uncurry3 square) [(v1, v2, p1), (v2, v3, p1), (v3,
 -- So, first are settings. They're size of the surface in our units (w h) then resolution of the surface (w h).
 -- Then the next two are the position of the camera, and the vector it's pointing in
 -- We assume for now that the camera is always level relative to (1, 0, 1) (No roll)
-data Camera a = Camera a a Int Int (V.Vector a) (V.Vector a)
+data Camera a = Camera a a Int Int (Vector a) (Vector a)
 
 -- This camera is currently orthographic, rather than perspective
 calc_ray_set camera@(Camera width height wres hres pos direction) = rays
 	where
 	-- This gives me the width axis of my image
 	-- It is acheived by a cross product of my looking direction and a vertical axis
-	width_axis = U.cross (V.fromList [0, 1, 0]) direction
+	width_axis = cross (fromList [0, 1, 0]) direction
 	-- This gives me the height axis of my image
-	height_axis = U.cross direction width_axis
+	height_axis = cross direction width_axis
 	-- This function computes a vector based on progress along a vector
 	partial_vector vec steps current_step = (on (/) fromIntegral current_step steps) |* vec
 	-- This function computes the position of a ray given its place in the matrix
-	ray_pos x y = (partial_vector (width |* width_axis) wres x) + (partial_vector (height |* height_axis) hres y) + pos + (V.fromList [-width / 2, -height/2, 0])
+	ray_pos x y = (partial_vector (width |* width_axis) wres x) + (partial_vector (height |* height_axis) hres y) + pos + (fromList [-width / 2, -height/2, 0])
 	-- Now I compute the associative list of rays using buildMatrix
 	rays = map (Ray direction) $ map (uncurry ray_pos) $ map swap $ range ((1, 1), (hres, wres))
 
 -- This makes a crude ASCII image
 -- I just assume that there are (x * y) items in bools
-make_shitty_image x y bools = L.intercalate "\n" $ takes x $ map pixel bools
+make_shitty_image x y bools = intercalate "\n" $ takes x $ map pixel bools
 	where
 	pixel True = '#'
 	pixel False = ' '
@@ -99,6 +99,6 @@ main = putStrLn $ make_shitty_image wres hres $  map (\x -> ray_intersect_mesh x
 	wres = 80
 	hres = 20
 	test_rays :: [Ray Double]
-	test_rays = calc_ray_set $ Camera 4 4 wres hres (3 V.|> [0,0, -3]) (3 V.|> [0,0,1])
+	test_rays = calc_ray_set $ Camera 4 4 wres hres (3 |> [0,0, -3]) (3 |> [0,0,1])
 	test_cube :: [Triangle Double]
-	test_cube = cube (3 V.|> [1, 0, 0]) (3 V.|> [0, 1, 0]) (3 V.|> [0, 0, 1]) (3 V.|> [0, 0, 0])
+	test_cube = cube (3 |> [1, 0, 0]) (3 |> [0, 1, 0]) (3 |> [0, 0, 1]) (3 |> [0, 0, 0])
