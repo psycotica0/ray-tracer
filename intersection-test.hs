8032c5c81dbe777ffdb03b5491343a7b10bf8bae
Christopher Vollick <psycotica0@gmail.com>
2012-10-28 11:45:30 -0400

    Replaced My Uses of Scalar with |*
    I've made the |* operator for multiplying a scalar by a vector.
    I feel like it's cleaned up the code some.

--- intersection-test.hs
+++ intersection-test.hs
@@ -15,12 +15,12 @@ scalar a = V.buildVector 3 $ const a
 -- This represents a plane of the form p = ax + by + c
 data Plane a = Plane (V.Vector a) (V.Vector a) (V.Vector a) deriving (Show)
 
-calc_plane (Plane a b c) n1 n2 = (a * (scalar n1)) + (b * (scalar n2)) + c
+calc_plane (Plane a b c) n1 n2 = (n1 |* a) + (n2 |* b) + c
 
 -- This represents a ray of the form r = ax + b
 data Ray a = Ray (V.Vector a) (V.Vector a) deriving (Show)
 
-calc_ray (Ray a b) n1 = (a * (scalar n1)) + b
+calc_ray (Ray a b) n1 = (n1 |* a) + b
 
 -- Each of these Vectors represent a point in 3-space
 data Triangle a = Triangle (V.Vector a) (V.Vector a) (V.Vector a) deriving (Show)
@@ -31,7 +31,7 @@ triangle_to_plane (Triangle p1 p2 p3) = Plane (p2 - p1) (p3 - p1) p1
 
 ray_plane_intersection ray@(Ray r1 r2) plane@(Plane p1 p2 p3) = result undefined
 	where
-	mat = M.fromColumns [p1, p2, (scalar (-1)) * r1]
+	mat = M.fromColumns [p1, p2, -1 |* r1]
 	solns = A.linearSolve mat (M.asColumn (r2 - p3))
 	n = solns M.@@> (2,0)
 	-- The parameter here is a hack to get around the fact that it doesn't pattern match if there are no parameters
@@ -60,7 +60,7 @@ square v1 v2 p1 = map (uncurry3 Triangle) [(p1, p1 + v1, p1 + v1 + v2), (p1 + v1
 cube v1 v2 v3 p1 = concatMap (uncurry3 square) [(v1, v2, p1), (v2, v3, p1), (v3, v1, p1), (neg v1, neg v2, p2), (neg v2, neg v3, p2), (neg v3, neg v1, p2)]
 	where
 	p2 = p1 + v1 + v2 + v3
-	neg = (*) $ scalar (-1)
+	neg = ((-1) |*)
 
 -- This is a camera. It's got a bunch of crap
 -- So, first are settings. They're size of the surface in our units (w h) then resolution of the surface (w h).
