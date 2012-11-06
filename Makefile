ef91dfb4f5fdad1641dccab65fb88e55333550bb
Christopher Vollick <psycotica0@gmail.com>
2012-11-06 17:45:56 -0500

    Added SDL Test
    Much better display.
    I can actually see that it appears reasonable.
    
    It takes SO LONG, though.
    That's a good baseline, though.

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 .PHONY: clean
+
+sdl_test: sdl_test.hs Raytracer/Camera.hs Raytracer/Geometry.hs
+	ghc sdl_test
+
 intersection-test: intersection-test.hs Raytracer/Camera.hs Raytracer/Geometry.hs
 	ghc intersection-test.hs
 
 clean:
-	$(RM) *.o Raytracer/*.o *.hi Raytracer/*.hi intersection-test
+	$(RM) *.o Raytracer/*.o *.hi Raytracer/*.hi intersection-test sdl_test
---
+++ sdl_test.hs
@@ -0,0 +1,47 @@
+import Graphics.UI.SDL.General (withInit, InitFlag(InitEverything))
+import Graphics.UI.SDL.Types (Surface, surfaceGetPixels, surfaceGetPixelFormat, SurfaceFlag(SWSurface))
+import Graphics.UI.SDL.Video (setVideoMode, mapRGB)
+import qualified Graphics.UI.SDL.Video as V (flip)
+import Graphics.UI.SDL.Events (Event(Quit), waitEvent)
+import Foreign.Ptr (castPtr)
+import Data.Array (elems)
+import Foreign.Marshal.Array (pokeArray)
+import Control.Monad (liftM)
+
+import Data.Packed.Vector ((|>))
+
+import Raytracer.Geometry (cube)
+import Raytracer.Camera (Camera(Camera), calculate_rays, fire_rays)
+
+wres = 400
+hres = 300
+color_depth = 32
+
+test_camera = Camera 4 4 wres hres (3 |> [-3, -6, -3]) (3 |> [1,1,1])
+test_rays = calculate_rays test_camera
+test_cube = cube (3 |> [2, 0, 0]) (3 |> [0, 2, 0]) (3 |> [0, 0, 2]) (3 |> [0, 0, 0])
+
+compute_pixels surface = (mapM pixel).elems
+	where
+	pixel_format = surfaceGetPixelFormat surface
+	pixel (Just _) = mapRGB pixel_format 127 127 127
+	pixel Nothing = mapRGB pixel_format 0 0 0
+
+array_to_surface surface array = array_ptr >>= ((flip pokeArray) array)
+	where
+	array_ptr = liftM castPtr $ surfaceGetPixels surface
+
+wait_to_quit _ = waitEvent >>= handle
+	where
+	handle Quit = return ()
+	handle _ = wait_to_quit ()
+
+render = do
+	screen <- setVideoMode wres hres color_depth [SWSurface]
+	pixels <- compute_pixels screen $ fire_rays (calculate_rays test_camera) test_cube
+	array_to_surface screen pixels
+	V.flip screen
+
+
+main = withInit [InitEverything] $ render >>= wait_to_quit
+
