5efa2558f92763533f1b214f8909c2cd38604e2e
Christopher Vollick <psycotica0@gmail.com>
2012-12-23 15:57:53 -0500

    Different Frames
    This were seeming a little weird, so I put something together to build a
    few frames and allow me to see them.
    
    Things are weird.
    I think my whole projection thing is broken...

--- sdl_test.hs
+++ sdl_test.hs
@@ -3,13 +3,17 @@ import Graphics.UI.SDL.Types (Surface, surfaceGetPixels, surfaceGetPixelFormat, 
 import Graphics.UI.SDL.Video (setVideoMode, mapRGB)
 import qualified Graphics.UI.SDL.Video as V (flip)
 import Graphics.UI.SDL.WindowManagement (setCaption)
-import Graphics.UI.SDL.Events (Event(Quit), waitEvent)
+import Graphics.UI.SDL.Events (Event(Quit, KeyDown), waitEvent)
+import Graphics.UI.SDL.Keysym (Keysym(Keysym), SDLKey(SDLK_SPACE))
 import Foreign.Ptr (castPtr)
 import Data.Array (elems)
 import Foreign.Marshal.Array (pokeArray)
 import Control.Monad (liftM)
 
+import Control.Applicative ((<*>))
+
 import Data.Maybe (fromJust, isJust)
+import Data.List (intersperse)
 
 import Data.Packed.Vector ((|>))
 
@@ -20,8 +24,10 @@ wres = 400
 hres = 300
 color_depth = 32
 
-test_camera = Camera 4 3 wres hres (3 |> [-3, -6, -3]) (3 |> [1,1,1])
-test_rays = calculate_rays test_camera
+test_camera = Camera 4 3 wres hres -- (3 |> [-3, -6, -3]) (3 |> [1,1,1])
+test_positions = map (3 |>) [[1, 1, -1]] --, [2, 1, -1], [2, 2, -1], [2, 2, -2]]
+test_directions = map (3 |>) [[0,0,1], [1,0,1]] --, [-1, 0, 1], [0,1,1], [0,-1,1]]
+
 test_cube = cube (3 |> [2, 0, 0]) (3 |> [0, 2, 0]) (3 |> [0, 0, 2]) (3 |> [0, 0, 0])
 
 transpose (a, b) (c, d) v = (((v-c) * (b-a)) / (d-c)) + a
@@ -31,7 +37,7 @@ compute_pixels surface array = mapM (pixel (minimum just_list, maximum just_list
 	list = elems array
 	just_list = map fromJust $ filter isJust list
 	pixel_format = surfaceGetPixelFormat surface
-	pixel range (Just n) = greyscale $ round $ transpose (256, 0) range n
+	pixel range (Just n) = greyscale 127 -- TODO: If it's flat, we get all black... $ round $ transpose (255, 0) range n
 	pixel _ Nothing = greyscale 0
 	greyscale a = mapRGB pixel_format a a a
 
@@ -44,11 +50,16 @@ wait_to_quit _ = waitEvent >>= handle
 	handle Quit = return ()
 	handle _ = wait_to_quit ()
 
+wait_to_space _ = waitEvent >>= handle
+	where
+	handle (KeyDown (Keysym SDLK_SPACE _ _)) = return ()
+	handle _ = wait_to_space ()
+
 render _ = do
 	screen <- setVideoMode wres hres color_depth [SWSurface]
-	pixels <- compute_pixels screen $ fire_rays (calculate_rays test_camera) test_cube
-	array_to_surface screen pixels
+	pixels <- mapM (compute_pixels screen) $ fmap ((flip fire_rays) test_cube) $ fmap calculate_rays $ fmap test_camera test_positions <*> test_directions
+	sequence $ intersperse (V.flip screen >>= wait_to_space) $ map (array_to_surface screen) pixels
 	V.flip screen
 
-main = withInit [InitVideo, InitEventthread] $ setCaption "SDL Test" "" >>= render >>= wait_to_quit
+main = withInit [InitVideo, InitEventthread] $ setCaption "SDL Test" "" >>= render >> putStrLn "Ready To Quit" >>= wait_to_quit
 
